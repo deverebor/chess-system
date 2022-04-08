2d2198a710925706e93e8ef4cd0ada80f613e42b
Lucas Souza <lucasp.sdev@gmail.com>
2022-04-08 12:30:27 -0300

    feat: implement turns for each player

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,11 +7,23 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
+    private int turn;
+    private Color currentPlayer;
     private final Board board;
     
     public ChessMatch() {
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+    
+    public int getTurn() {
+        return turn;
+    }
+    
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
     
     public ChessPiece[][] getPieces() {
@@ -22,6 +34,11 @@ public class ChessMatch {
             }
         }
         return chessMatriz;
+    }
+    
+    private void nextTurn() {
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
     
     public boolean[][] possibleMoves(ChessPosition sourcePosition) {
@@ -38,6 +55,8 @@ public class ChessMatch {
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         
+        nextTurn();
+        
         return (ChessPiece)capturedPiece;
     }
     
@@ -52,6 +71,9 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if(!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
+        }
+        if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+            throw new ChessException("The chosen piece is not yours");
         }
         if(!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen piece");
