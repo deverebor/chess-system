829218e1abc06fe3eaabf35b5534e626cdc0600d
Lucas Souza <lucasp.sdev@gmail.com>
2022-04-08 10:44:19 -0300

    feat: create possibles moves in one piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private final Board board;
     
@@ -9,7 +9,23 @@ public class Piece {
         position = null;
     }
     
-    protected Board getBoard() {
-        return board;
+    protected Board getBoard() { return board; }
+    
+    public abstract boolean[][] possibleMoves();
+    
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+    
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] matriz = possibleMoves();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if (matriz[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
